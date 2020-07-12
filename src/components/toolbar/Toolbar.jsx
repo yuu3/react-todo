240e50dbfcc5dd8a378d827aca000901e333e6b7
yuu3 <yunosuke0802@icloud.com>
2020-07-13 01:31:01 +0900

    update Toolbar.jsx

--- src/components/toolbar/Toolbar.jsx
+++ src/components/toolbar/Toolbar.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-function Toolbar() {
+export const Toolbar = () => {
   return (
     <header className="w-full px-8 py-4 shadow-main bg-main text-white fixed top-0 left-0">
-      <h1 className="font-medium">React Calendar Toolbar</h1>
+      <h1 className="font-medium">React Todo Toolbar</h1>
     </header>
   )
 }
-
-export default Toolbar
