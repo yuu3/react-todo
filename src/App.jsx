de9b52e1e51477283e79feb8d7d43a4c70abfcc5
yuu3 <yunosuke0802@icloud.com>
2020-07-19 14:22:24 +0900

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, {
+  useState
+} from 'react'
 import { Toolbar } from './components/toolbar/Toolbar'
 import { TodoList } from './components/todoList/TodoList'
 import { ModalTodoCreate } from './components/modal/ModalTodoCreate'
